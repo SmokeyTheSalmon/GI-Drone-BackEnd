dbd1f51b4c52b63ceb61a304cadc14611496d94c
SmokeytheSalmon <Roelvanbakkum@gmail.com>
2019-06-05 14:46:06 +0200

    Backend structure van Stef overgeschreven

---
+++ GI-Drone-BackEnd/GI-Drone-BackEnd/src/main/java/com/droneboys/GIDroneBackEnd/domain/Pakket.java
@@ -0,0 +1,81 @@
+package com.droneboys.GIDroneBackEnd.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Pakket {
+	@Id
+	
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	long id;
+	
+	private String naam, adres, postcode, stad, pakket, afzender, huidige_locatie;
+	private int gewicht;
+	
+	public Pakket() {}
+	public Pakket(String adres, String afzender) {
+		this.adres = adres;
+		this.afzender = afzender;
+	}
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNaam() {
+		return naam;
+	}
+	public void setNaam(String naam) {
+		this.naam = naam;
+	}
+	public String getAdres() {
+		return adres;
+	}
+	public void setAdres(String adres) {
+		this.adres = adres;
+	}
+	public String getPostcode() {
+		return postcode;
+	}
+	public void setPostcode(String postcode) {
+		this.postcode = postcode;
+	}
+	public String getStad() {
+		return stad;
+	}
+	public void setStad(String stad) {
+		this.stad = stad;
+	}
+	public String getPakket() {
+		return pakket;
+	}
+	public void setPakket(String pakket) {
+		this.pakket = pakket;
+	}
+	public String getAfzender() {
+		return afzender;
+	}
+	public void setAfzender(String afzender) {
+		this.afzender = afzender;
+	}
+	public String getHuidige_locatie() {
+		return huidige_locatie;
+	}
+	public void setHuidige_locatie(String huidige_locatie) {
+		this.huidige_locatie = huidige_locatie;
+	}
+	public int getGewicht() {
+		return gewicht;
+	}
+	public void setGewicht(int gewicht) {
+		this.gewicht = gewicht;
+	}
+	
+	
+}
---
+++ GI-Drone-BackEnd/GI-Drone-BackEnd/src/main/java/com/droneboys/GIDroneBackEnd/endpoint/PakketEndpoint.java
@@ -0,0 +1,81 @@
+package com.droneboys.GIDroneBackEnd.endpoint;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.droneboys.GIDroneBackEnd.domain.Pakket;
+import com.droneboys.GIDroneBackEnd.service.PakketService;
+
+public class PakketEndpoint {
+
+	@Autowired
+	PakketService pakketService;
+	
+	//Create
+	@PostMapping	
+	public ResponseEntity<Pakket> apiCreate(@RequestBody Pakket pakket) {
+		if (pakket.getId() != 0) {
+			return new ResponseEntity<> (HttpStatus.CONFLICT);
+		}
+		return new ResponseEntity<Pakket>(
+				pakketService.save(pakket), 
+				HttpStatus.OK);
+	}
+	
+	//Retrieve
+	@GetMapping 	
+	public ResponseEntity<Iterable<Pakket>> apiGetAll() {
+		return new ResponseEntity<Iterable<Pakket>>(
+				pakketService.findAll(), 
+				HttpStatus.OK);
+	}
+
+	@GetMapping (path ="{id}")
+	public ResponseEntity<Optional<Pakket>> apiGetById(
+			@PathVariable long id) {
+		Optional<Pakket> pakket = pakketService.findById(id);
+		return new ResponseEntity<>(
+				pakket, 
+				pakket.isPresent() 
+					? HttpStatus.OK 
+					: HttpStatus.NOT_FOUND);
+	}
+	//Update
+	@PutMapping(path="{id}")		
+	public ResponseEntity<Pakket> apiUpdate(
+			@PathVariable("id") long id, 
+			@RequestBody Pakket pakket) {
+		if (pakket == null || pakket.getId() != id)
+			return new ResponseEntity<>(
+					HttpStatus.BAD_REQUEST);
+
+		Optional<Pakket> oldPakket = pakketService.findById(pakket.getId());
+		if (!oldPakket.isPresent()) {
+			return new ResponseEntity<>(
+					HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<>(
+				pakketService.save(pakket),
+				HttpStatus.OK);
+	}
+	
+	//Delete
+	@DeleteMapping(path="{id}") 
+	public ResponseEntity<Pakket> apiDeleteById(@PathVariable("id") long id) {
+		if (!pakketService.findById(id).isPresent()) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		} else {
+			pakketService.deleteById(id);
+			return new ResponseEntity<>(HttpStatus.OK);
+		}
+	}
+}

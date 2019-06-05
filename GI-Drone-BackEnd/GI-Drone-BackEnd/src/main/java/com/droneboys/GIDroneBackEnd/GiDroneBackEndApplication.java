5b283b509128a01aa8e08cc7df5e01a128d2c3b7
SmokeytheSalmon <Roelvanbakkum@gmail.com>
2019-06-05 13:08:44 +0200

    initialized backend framework thing

---
+++ GI-Drone-BackEnd/GI-Drone-BackEnd/src/main/java/com/droneboys/GIDroneBackEnd/GiDroneBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.droneboys.GIDroneBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiDroneBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiDroneBackEndApplication.class, args);
+	}
+
+}
